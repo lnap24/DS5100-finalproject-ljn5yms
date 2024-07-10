f68cd0d8e0ed8bbb6bce69e5b7d677a551684726
LukeJoseph_Napolitano <luke.nap@comcast.net>
2024-07-09 22:28:50 -0400

    updating monte carlo

--- montecarlo/montecarlo.py
+++ montecarlo/montecarlo.py
@@ -1,10 +1,44 @@
 class Die():
+    
     def __init__(self):
         pass
+    
+    def side_weight_change(self):
+        pass
+    
+    def roll(self):
+        pass
+    
+    def current_state(self):
+        pass
+    
 class Game():
+    
     def __init__(self):
         pass
+    
+    def play(self):
+        pass
+    
+    def results(self):
+        pass
+    
 class Analyzer():
+    
     def __init__(self):
         pass
+    
+    def jackpot(self):
+        pass
+    
+    def face_count(self):
+        pass
+    
+    def combo_count(self):
+        pass
+    
+    def perm_count(self):
+        pass
+    
+    
 
